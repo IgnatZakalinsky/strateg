639000b5ad97b99c14fe9c7e2fd62cb326dd6b01
IgnatZakalinsky <ai73a@yandex.ru>
2018-02-25 02:40:05 +0200

    + info

--- src/Base.java
+++ src/Base.java
@@ -1,3 +1,10 @@
-public class Base {
+/**
+ * информация о базах <br>
+ *     - {@link Base#cu} - прочность Центра управления <br>
+ *     - {@link Base#pm} - Производственные мощности <br>
+ *     - {@link Base#o} - оборона <br>
+ *     - {@link Base#a} - атакующие подразделения <br>
+ */
+class Base {
     long cu = 16, o = 0, a = 0, pm = 1;
 }
